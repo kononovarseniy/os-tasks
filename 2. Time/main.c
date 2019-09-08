4d6e3074e0cf509864c0f4e31645b4761f51aff5
Arseniy Kononov <kononovarseniy@gmail.com>
2019-09-08 10:59:26 +0700

    Task #2. Time

---
+++ 2. Time/main.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+extern char *tzname[];
+
+void print_time(const time_t *t, char *tz_var) {
+    struct tm *sp;
+    
+    putenv(tz_var);
+    
+    sp = localtime(t);
+    printf("localtime (%s):\n\t", tz_var);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        (sp->tm_year % 100), sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+}
+
+int main()
+{
+    time_t now;
+
+    (void) time( &now );
+    printf("ctime: %s", ctime( &now ) );
+
+    print_time(&now, "TZ=");
+    print_time(&now, "TZ=<+07>-7");
+    print_time(&now, "TZ=:Asia/Novosibirsk");
+    print_time(&now, "TZ=PST8PDT");
+    print_time(&now, "TZ=:America/Los_Angeles");
+    
+    return 0;
+}
